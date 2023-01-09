90bc37059311dee4e3286645a3d40aa6ef5abb73
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-01-09 09:50:35 +0100

    tweaked some webpack stuf

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
             },
             {
                 test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                use: ['style-loader', 'css-loader'],
             },
             {
                 test: /\.html$/i,
